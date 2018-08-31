c281a14d42aae8ea37419637f2273675aa55b0b3
Prajakta <ppmande29@gmail.com>
2018-08-31 17:22:14 +0530

    prajakta -demo theme
    31/8/18

---
+++ Prajakta/websites/demo/demo/wp-content/themes/dental2/header.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button> 
+
+
+            <a class="navbar-brand" href="#page-top">AVISCO</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#portfolio">Home</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="about.html">About</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#services">services</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#contact">Contact</a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+<header id="page-top">
+    
+    <div class="container">
+        <div class="row">
+             
+            <div class="col-lg-12">
+               
+                <img class="img-responsive" src="http://ironsummitmedia.github.io/startbootstrap-freelancer/img/profile.png" alt="">
+                <div class="intro-text">
+                    <div class="banner_animate">
+                 
+                      <div class="w3-container w3-center w3-animate-top">
+                      <h2> AVISCO</h2>
+
+                   <!-- <hr class="star-light">  -->
+                    </div>
+                    </div>
+               <!--  <p class="skills">Landing Page Layout</p>
+                    <p class="skills">Scroll to see the effect</p> -->
+                </div>
+            </div>
+            <div class="row">
+
+              <div class="col-sm-6">
+                  <div class="banner_animate">
+                 
+                      <div class="w3-container w3-center w3-animate-left">
+                <h3>Book Appointment</h3>
+              </div>
+            </div>  
+                 <form action="http://www.practo.com" target="_blank">
+               <button type="submit" class="btn btn-primary btn-lg">Click here
+                                </button>
+              </div>
+                  
+    
+</form>
+               <div class="col-sm-6"x> <h3 style="text-align:center;">
+              <div class="banner_animate">
+                 
+                      <div class="w3-container w3-center w3-animate-bottom">
+               Opning time</h3>
+                <p>Monday to Saturday:9am to 5pm</p>
+                  <p>   Sunday: closed</p>
+                </div>
+              </div>
+             
+              </div>
+            </div>
+            
+        </div>
+        </div>
+  
+</header>
+</body>
+
+</html>
---
+++ Prajakta/websites/demo/demo/wp-content/themes/dental4/front-page.php
@@ -0,0 +1,327 @@
+<?php
+/**
+ * The front page template file
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<div class="content-wrapper">
+    <section class="primary" id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="banner_animate">
+                 
+                      <div class="w3-container w3-center w3-animate-bottom">
+                    <h2>Home</h2>
+                  </div>
+                </div>
+                   <!--  <hr class="star-light"> -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <!-- mage slider -->
+                 <!--    <img src="first/assets/images/images1.jpg">    
+                </div>
+                <div class="col-sm-4">
+                    <img src="first/assets/images/images2.jpg">    
+                </div>
+                <div class="col-sm-4">
+                    <img src="first/assets/images/images3.jpg">    
+                </div>
+            </div> -->
+            <!-- <image slider ends> -->
+
+
+
+                <!-- image slider from w3shools -->
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+   
+
+<div class="container">
+ 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="first/assets/images/images1.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="first/assets/images/images2.jpg" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="first/assets/images/images3.jpg" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <br>
+  <h1 style="text-align: center;">Welcome to the White Arch Dental Centre</h1>
+  <h5>The White Arch Dental Centre is a multi-speciality laser and microscope enhanced dental clinic, founded by Dr Vivek Hegde – one of the foremost professionals in his field.
+At The White Arch, we offer a wide range of dental treatments, from general to much more complex microdentistry, cosmetic and minimally invasive laser dentistry treatments.
+We have three high-end operatories, each equipped with the best dental technology, sourced from around the world.
+We are committed to creating a calm, pleasant atmosphere for the patients, while maintaining the strictest hygiene standards.
+We also provide a training centre for dentists, to teach them the latest advancements in dentistry.
+Please explore this website to know more about The White Arch Dental Centre and the people behind it.
+On average, you'll spend 38.5 days brushing your teeth in your lifetime
+
+</h5>
+</div>
+</div>
+
+                 
+               <!--  image slider from w3shools ends --> -->
+        </div>
+    </section>
+ 
+    <!-- ABOUT SEESION START -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+
+ 
+
+
+   <div style="color: black">               
+  <div class="col-lg-12 text-center">
+
+                    <h2>About</h2>
+                   <!--  <hr class="star-light"> -->
+               <!--  </div> -->
+          
+            <!-- <div class="row"> -->
+                <!-- <div class="col-lg-4 col-lg-offset-2"> -->
+                   <h4 style="text-align: center">Team of ABC Dental Clinic</h4>
+                    <p>THe ABC dental clinic offers more than two decades of collective professional experiance. Meet the team</p>
+                </div>
+
+                   
+                </div>
+
+            </div>
+            <div class="row">
+              <div class="col-lg-6" style="background-color: red"></div>
+            <div class="col-lg-6" style="background-color: green"></div>
+            </div>
+        </div>
+     
+    
+ </section> 
+  
+
+<!-- ABOUT SESSION ENDS -->
+
+<!-- SERVICES SECTION START -->
+
+
+<style type="text/css">
+
+marquee{
+    width: 50%;
+}
+</style>
+
+
+
+      <section id="services" style="background-color:white">
+        <!-- <div id="grad1" class="effect2"> -->
+           <div class="container">
+            <div class="row">
+                
+           <div class="col-lg-12 text-center">
+              <h2 style="color: black">Services</h2>
+              <br>
+              <p> Here at the ABC Dental clinic, we cover a whole gamut of dental treatments and services. We go to any lenghts possible to make sure your smile stay bright and healthier than ever. </p>
+          <!--   <hr class="star-light"> -->
+         </div>
+       </div>
+       <div class="container-fluid "> 
+
+               
+                 <div class="row">
+    <div class="col-sm-6">
+       
+<!-- <div class="well-sm well1">Root Canal Therapy</div>
+<br>
+<div class="well-sm well1">Implant Dentisry</div>
+<br>
+<div class="well-sm well1" >Microsurgery</div>
+<br>
+<div class="well-sm well1" >Minor Oral Surgery</div>
+<br>
+<div class="well-sm well1" >Peizosurgery</div>
+<br>
+<div class="well-sm well1" >Periodontal Therapy</div>
+<br> -->
+      
+
+       <!--    **************** -->
+       <div class="panel  panel-pricing" style="background-color:  #18BC9C">
+                        <div class="panel-heading">
+                            
+                            <h3 style="text-align: center;">Services</h3>
+                        </div>
+                       <!--  <div class="panel-body text-center" style="background-color:white">
+                            <p><strong>Root Canal Therapy</strong></p>
+                        </div> -->
+                        <ul class="list-group ">
+                            <li class="list-group-item"><i class="fa fa-check"></i> Root Canal Therapy</li>
+                            <li class="list-group-item"><i class="fa fa-check"></i> Implant Dentisry</li>
+                            <li class="list-group-item"><i class="fa fa-check"></i> Microsurgery</li>
+                            <li class="list-group-item"><i class="fa fa-check"></i>Minor Oral Surgery</li>
+                            <li class="list-group-item"><i class="fa fa-check"></i> Peizosurgery</li>
+                            <li class="list-group-item"><i class="fa fa-check"></i>Periodontal Therapy
+                            </li>
+                           
+                            <li class="list-group-item"><i class="fa fa-check"></i> Restorative Dentisry</li>
+                             <li class="list-group-item"><i class="fa fa-check"></i>Prventive DEntistry</li>
+                              <li class="list-group-item"><i class="fa fa-check"></i> Microsurgery</li>
+
+                        </ul>
+                       <!--  <div class="panel-footer">
+                            <a class="btn btn-lg btn-block btn-danger" href="#">BUY NOW!</a>
+                        </div> -->
+                    </div>
+                </div>
+     <!--   ****************************** -->
+
+ 
+    <div class="col-sm-6 text-center"> 
+   
+               <div class="panel  panel-pricing" style="background-color:  #18BC9C">
+                        <div class="panel-heading">
+                            
+                            <h3 style="text-align: center;">Services</h3>
+                        </div>
+                       <!--  <div class="panel-body text-center" style="background-color:white">
+                            <p><strong>Root Canal Therapy</strong></p>
+                        </div> -->
+
+                        <ul class="list-group">
+                            <li class="list-group-item"><i class="fa fa-check"></i><a href="first/assets/rootcanal.html"> Root Canal Therapy</li></a>
+                            <li class="list-group-item"><i class="fa fa-check"></i> <a href="#">
+                            Implant Dentisry</li></a> 
+                            <li class="list-group-item"><i class="fa fa-check"></i> Microsurgery</li>
+                            <li class="list-group-item"><i class="fa fa-check"></i>Minor Oral Surgery</li>
+                            <li class="list-group-item"><i class="fa fa-check"></i> Peizosurgery</li>
+                            <li class="list-group-item"><i class="fa fa-check"></i>Periodontal Therapy
+                            </li>
+                           
+                            <li class="list-group-item"><i class="fa fa-check"></i> Restorative Dentisry</li>
+                             <li class="list-group-item"><i class="fa fa-check"></i>Prventive DEntistry</li>
+                              <li class="list-group-item"><i class="fa fa-check"></i> Microsurgery</li>
+
+                        </ul>
+                       <!--  <div class="panel-footer">
+                            <a class="btn btn-lg btn-block btn-danger" href="#">BUY NOW!</a>
+                        </div> -->
+                    </div>
+                </div>
+  
+
+     <br><br
+
+    </div>
+   
+  </div>
+           
+        </div>
+    </div>
+    </section>
+
+<!-- SERVICES SESSION ENDS -->
+
+    <section  id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contact</h2>
+                  <!--   <hr class="star-primary"> -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+                    <form name="sentMessage" id="contactForm" novalidate="">
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name" id="name" required="" data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email Address</label>
+                                <input type="email" class="form-control" placeholder="Email Address" id="email" required="" data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Phone Number</label>
+                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required="" data-validation-required-message="Please enter your phone number.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Message</label>
+                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required="" data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button type="submit" class="btn btn-success btn-lg">Send
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer();
