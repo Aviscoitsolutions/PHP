0acbc0199079041269926e0871462c9ccfdf3258
Ilyas <ilyaskamptee@gmail.com>
2018-09-18 18:51:49 +0530

    ilyas
    Uploaded  18-9-2018

--- ilyas/dental1 7-9-2018/wp-content/themes/twentyseventeen/template-parts/add_submit.php
+++ ilyas/dental1 7-9-2018/wp-content/themes/twentyseventeen/template-parts/add_submit.php
@@ -7,22 +7,133 @@ include("connect.php");
 $sitepanel=$_POST["flag"];
 $name=$_POST["test_name"];
 $img=$_FILES["test_img"]["name"];
-//echo $_FILES["test_img"]["size"];
+$size=$_FILES["test_img"]["size"];
 $desc=$_POST["test_desc"];
 $val=$_POST["test_val"];
 $movepath=wp_upload_dir();
 $upload=$movepath['basedir'];
-$imgpath=$upload."/testimonials/".$img;
 $home=$movepath['url'];
+$imgext=explode(".",$img);
+ $imgext[0];
+ $imgext[2];
+ $imgext[1];
 
-$moveimage=move_uploaded_file($_FILES["test_img"]["tmp_name"],$imgpath);
-echo $insert="insert into wp_testimonials(test_name,test_desc,test_img,test_value,test_date) values('$name','$desc','$img','$val',now())";
-$insertq=mysqli_query($con,$insert);
-if($sitepanel=='sitepanel')
-{
-wp_redirect($home."/add-site-testimonials/?flag=success");
-} else{
-wp_redirect($home."/add-testimonials/?flag=success");
+/*****************************************THIS  IS FOR SITE PANEL PAGE****************************/
+if($sitepanel == 'sitepanel'){		
+		
+		if($imgext[1] == 'jpg')
+		{
+			//3145728
+			//exit;
+			if($size < 3145728)
+			{
+					$imgpath=$upload."/testimonials/".$img;
+					//exit;
+					$moveimage=move_uploaded_file($_FILES["test_img"]["tmp_name"],$imgpath);
+					$insert="insert into wp_testimonials(test_name,test_desc,test_img,test_value,test_date) values('$name','$desc','$img','$val',now())";
+					$insertq=mysqli_query($con,$insert);
+					
+					if($sitepanel=='sitepanel')
+					{
+					wp_redirect($home."/add-site-testimonials/?flag=success");exit;
+					} 
+					else
+					{
+					wp_redirect($home."/add-testimonials/?flag=success");exit;
+					}
+
+
+			}
+			else
+			{
+				wp_redirect($home."/add-site-testimonials/?flag=simg");	exit;
+
+			}
+
+		}
+		else
+		{
+			wp_redirect($home."/add-site-testimonials/?flag=rimg");	exit;	
+		}
+
+		
+		
+} else {
+
+/*****************************************THIS  IS FOR SITE PANEL PAGE****************************/
+/*****************************************THIS  IS FOR ADMIN PANEL PAGE****************************/
+
+	
+		
+		if($imgext[1] == 'jpg')
+		{
+			//3145728
+			//exit;
+			if($size < 3145728)
+			{
+					$imgpath=$upload."/testimonials/".$img;
+					//exit;
+					$moveimage=move_uploaded_file($_FILES["test_img"]["tmp_name"],$imgpath);
+					$insert="insert into wp_testimonials(test_name,test_desc,test_img,test_value,test_date) values('$name','$desc','$img','$val',now())";
+					$insertq=mysqli_query($con,$insert);
+					if($sitepanel=='sitepanel')
+					{
+					wp_redirect($home."/add-testimonials/?flag=success");exit;
+					} 
+					else
+					{
+					wp_redirect($home."/add-testimonials/?flag=success");exit;
+					}
+
+
+			}
+			else
+			{
+				wp_redirect($home."/add-testimonials/?flag=simg");	exit;
+
+			}
+
+		}
+		else
+		{
+			wp_redirect($home."/add-testimonials/?flag=rimg");	exit;	
+		}
+
+		
+		
 }
+/*****************************************THIS  IS FOR ADMIN PANEL PAGE****************************/
 
+
+
+
+
+
+
+// if($img!='jpg' && $sitepanel == 'sitepanel'){		
+// 		wp_redirect($home."/add-site-testimonials/?flag=rimg");		
+// }
+// $imgpath=$upload."/testimonials/".$img;
+// exit;
+// $moveimage=move_uploaded_file($_FILES["test_img"]["tmp_name"],$imgpath);
+// $insert="insert into wp_testimonials(test_name,test_desc,test_img,test_value,test_date) values('$name','$desc','$img','$val',now())";
+// $insertq=mysqli_query($con,$insert);
+// if($sitepanel=='sitepanel')
+// {
+// wp_redirect($home."/add-site-testimonials/?flag=success");
+// } 
+// else
+// {
+// wp_redirect($home."/add-testimonials/?flag=success");
+// }
+/*if($size < 1725830){
+		if($sitepanel=='sitepanel')
+		{
+		wp_redirect($home."/add-site-testimonials/?flag=simg");
+		} 
+		else
+		{
+		wp_redirect($home."/add-testimonials/?flag=simg");
+		}
+}*/
 ?>
